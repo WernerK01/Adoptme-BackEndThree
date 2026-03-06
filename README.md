d801abc5c5b53f85b977012914b6b0dcd0e3941f
sachiko220 <sachikoreal01@gmail.com>
2026-03-05 23:46:14 -0300

    .

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Adoptme
 
 BackEnd de un sistema para adoptar mascotas. Primera entrega formal.
+[Enlace para Docker Hub](https://hub.docker.com/repository/docker/wernerk01/app-adoptme-main/general)
 
 ---
 
--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import 'dotenv/config';
+import swaggerUi from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
 
 import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
@@ -10,9 +12,9 @@ import sessionsRouter from './routes/sessions.router.js';
 import mockingRouter from './routes/mocks.router.js';
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URL || '').then(() => {
+mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log('Connected to MongoDB');
 }).catch((err) => {
     console.error('Error connecting to MongoDB:', err);
@@ -21,10 +23,30 @@ mongoose.connect(process.env.MONGO_URL || '').then(() => {
 app.use(express.json());
 app.use(cookieParser());
 
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Pet Adoption API',
+            version: '1.0.0',
+            description: 'API for managing pet adoptions',
+        },
+        servers: [
+            {
+                url: `http://localhost:${PORT}`,
+            },
+        ],
+    },
+    apis: ['./src/docs/*.yaml'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.use('/api/users', usersRouter);
 app.use('/api/pets', petsRouter);
 app.use('/api/adoptions', adoptionsRouter);
 app.use('/api/sessions', sessionsRouter);
 app.use('/api/mocks', mockingRouter);
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on http://localhost:3000\nAPI documentation available at http://localhost:3000/api-docs`));
